fbc1f415d41231e9e2e36d1a6dfa5639979a68f0
preetsohal <preetsohal@live.com>
2016-08-23 22:57:20 -0700

    looks like i completed therequirement

---
+++ app/data/images.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = [{
+  id: 1,
+  url: 'http://en.bcdn.biz/Images/2015/7/9/623243b4-6e86-4c45-9474-8b02f67143ed.jpg',
+  description: 'I am Lucy. i am full of cuteness.'
+}, {
+  id: 2,
+  url: 'http://kittentoob.toobnetwork.com/wp-content/uploads/sites/4/2013/04/Smiling_Cats_3.jpg',
+  description: 'Hi, i am Bella and i am adorable.'
+}, {
+  id: 3,
+  url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/07/smiling-cat-33__605.jpg',
+  description: 'Hello from Jasper. chao!'
+}, {
+  id: 4,
+  url: 'http://www.hi-wallpapers.com/uploads/image/201211/19/1353302466.jpg',
+  description: 'Hey there! Angel here.'
+},{
+  id: 5,
+  url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAji5R9-uixOMnptZAkKeLG-NRLglkC-dsUMHGhqOytJyXnAN4CQ',
+  description: 'you can\'t skip me. I am Toby.'
+}];
